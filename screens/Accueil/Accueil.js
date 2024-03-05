0dbcb0eee888819ac80ed1db38f494bdad5bc998
BeWolfff <berenice.wolf@yahoo.fr>
2024-03-05 17:32:20 +0100

    récupération de la liste des articles

--- screens/Accueil/Accueil.js
+++ screens/Accueil/Accueil.js
@@ -1,9 +1,22 @@
 import { Text, View } from "react-native";
 import AppStyles from "../../AppStyles";
+import { useEffect, useState } from "react";
 
 
 export default () => {
+
+    const [listeArticle, setListeArticle] = useState([]);
+
+    useEffect(() => {
+
+        fetch("https://dummyjson.com/products")
+            .then(result => result.json())
+            .then(result => setListeArticle(result.products));
+
+    }, []);
+
   const appStyles = AppStyles();
+
   return (
     <View style={[appStyles.container, appStyles.centered]}>
       <Text>Accueil</Text>
