80e67994eeed3d29f3987db93ed407d391c27eb9
BeWolfff <berenice.wolf@yahoo.fr>
2024-03-05 15:47:04 +0100

    add tab navigator

--- App.js
+++ App.js
@@ -1,28 +1,39 @@
 import { StatusBar } from "expo-status-bar";
-import { View } from "react-native";
+import { Text, View } from "react-native";
 import AppButton from "./components/AppButton/AppButton";
 import { SafeAreaProvider } from "react-native-safe-area-context";
 import AppStyles from "./AppStyles";
-
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import { NavigationContainer } from "@react-navigation/native";
 export default function App() {
+  const NavigationPrincipale = createBottomTabNavigator();
   const EcranPrincipal = () => {
     const styles = AppStyles();
-
     const onPressClicMoi = () => {
       console.log("Hello !");
     };
-
     return (
-      <View style={[styles.container, styles.safeArea]}>
+      <View style={[styles.safeArea, styles.container]}>
         <AppButton title="Clic moi" onPress={onPressClicMoi}></AppButton>
         <StatusBar style="auto" />
       </View>
     );
   };
-
+  const EcranAutre = () => {
+    return (
+      <View>
+        <Text>Autre écran</Text>
+      </View>
+    );
+  };
   return (
     <SafeAreaProvider>
-      <EcranPrincipal></EcranPrincipal>
+      <NavigationContainer>
+        <NavigationPrincipale.Navigator>
+          <NavigationPrincipale.Screen name="main" component={EcranPrincipal} />
+          <NavigationPrincipale.Screen name="other" component={EcranAutre} />
+        </NavigationPrincipale.Navigator>
+      </NavigationContainer>
     </SafeAreaProvider>
   );
 }
