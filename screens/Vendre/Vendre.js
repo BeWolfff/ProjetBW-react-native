7c69fdea751b7cc629436034b49dcc8624c278a3
BeWolfff <berenice.wolf@yahoo.fr>
2024-03-15 10:59:38 +0100

    formulaire sur Vendre.js

--- screens/Vendre/Vendre.js
+++ screens/Vendre/Vendre.js
@@ -1,12 +1,44 @@
 import { Text, View } from "react-native";
 import AppStyles from "../../AppStyles";
+import { Input } from "@rneui/themed";
+import { Controller, useForm } from "react-hook-form";
 
 
 export default () => {
   const appStyles = AppStyles();
+
+  const {control, handleSubmit, formState: {errors}} = useForm();
+
   return (
     <View style={[appStyles.container, appStyles.centered]}>
-      <Text>Vendre</Text>
+      <Controller 
+      control={control}
+      name="nom"
+      defaultValue=""
+      rules={{required: true}}
+      render={({field:{onChange, onBlur,value}})=> (
+        <Input label="Nom" 
+        style={{margin:0, padding:0}} 
+        onBlur={onBlur}
+        onChangeText={(text) => onChange<(text)}
+        value={value}      
+        />
+      )}
+      ></Controller>
+      control={control}
+      name="Designation"
+      defaultValue=""
+      rules={{required: true}}
+      render={({field:{onChange, onBlur,value}})=> (
+        <Input label="Designation" 
+        style={{margin:0, padding:0}} 
+        multiline={true}
+        numberOfLines={4}
+        onBlur={onBlur}
+        onChangeText={(text) => onChange<(text)}
+        value={value}      
+        />
+      )}
     </View>
   );
 };
