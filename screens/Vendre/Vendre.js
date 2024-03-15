ee39953c4ed1821aac4f94c81ee27f31c10dbc04
BeWolfff <berenice.wolf@yahoo.fr>
2024-03-15 11:14:58 +0100

    Correction formulaire Vendre

--- screens/Vendre/Vendre.js
+++ screens/Vendre/Vendre.js
@@ -2,12 +2,15 @@ import { Text, View } from "react-native";
 import AppStyles from "../../AppStyles";
 import { Input } from "@rneui/themed";
 import { Controller, useForm } from "react-hook-form";
+import AppButton from "../../components/AppButton/AppButton";
 
 
 export default () => {
   const appStyles = AppStyles();
 
   const {control, handleSubmit, formState: {errors}} = useForm();
+
+  const onAjout = (offre) => console.log(offre)
 
   return (
     <View style={[appStyles.container, appStyles.centered]}>
@@ -20,25 +23,30 @@ export default () => {
         <Input label="Nom" 
         style={{margin:0, padding:0}} 
         onBlur={onBlur}
-        onChangeText={(text) => onChange<(text)}
+        onChangeText={(text) => onChange(text)}
         value={value}      
         />
       )}
-      /><Controller
+      />
+      <Controller
       control={control}
-      name="Designation"
+      name="Description"
       defaultValue=""
       rules={{required: true}}
       render={({field:{onChange, onBlur,value}})=> (
-        <Input label="Designation" 
+        <Input label="Description" 
         style={{margin:0, padding:0}} 
         multiline={true}
         numberOfLines={4}
         onBlur={onBlur}
-        onChangeText={(text) => onChange<(text)}
+        onChangeText={(text) => onChange(text)}
         value={value}      
         />
       )}/>
+
+      <AppButton title="Ajouter mon offre" onPress={handleSubmit(onAjout)} >
+
+      </AppButton>
     </View>
   );
 };
