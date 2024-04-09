9009e88c83690c5e4d43892dd3696c9a4e3fa0ed
BeWolfff <berenice.wolf@yahoo.fr>
2024-04-09 14:51:10 +0200

    ajout fonctions pour notification

--- App.js
+++ App.js
@@ -15,8 +15,67 @@ import Connexion from "./screens/Connexion/Connexion";
 import * as SecureStore from "expo-secure-store";
 import Inscription from "./screens/Inscription/Inscription";
 import Toast from "react-native-root-toast";
+import { useEffect } from "react";
+import { View, Platform } from "react-native";
+import * as Device from "expo-device";
+import * as Notifications from "expo-notifications";
+import Constants from "expo-constants";
+import { LogBox } from "react-native";
+
+
+
+
 
 export default () => {
+
+  useEffect(() => {
+    registerForPushNotificationsAsync().then((token) => {
+    console.log(token);
+    });
+    }, []);
+    
+
+  LogBox.ignoreLogs(["new NativeEventEmitter"]);
+Notifications.setNotificationHandler({
+handleNotification: async () => ({
+shouldShowAlert: true,
+shouldPlaySound: false,
+shouldSetBadge: false,
+}),
+});
+
+async function registerForPushNotificationsAsync() {
+  let token;
+  if (Platform.OS === "android") {
+  Notifications.setNotificationChannelAsync("default", {
+  name: "default",
+  importance: Notifications.AndroidImportance.MAX,
+  vibrationPattern: [0, 250, 250, 250],
+  });
+  }
+  if (Device.isDevice) {
+  const { status: existingStatus } =
+  await Notifications.getPermissionsAsync();
+  let finalStatus = existingStatus;
+  if (existingStatus !== "granted") {
+  const { status } = await Notifications.requestPermissionsAsync();
+  finalStatus = status;
+  }
+  if (finalStatus !== "granted") {
+  alert("Failed to get push token for push notification!");
+  return;
+  }
+  token = await Notifications.getExpoPushTokenAsync({
+  projectId: Constants.expoConfig.extra.eas.projectId,
+  });
+  } else {
+  alert("Must use physical device for Push Notifications");
+  }
+  return token.data;
+  }
+  
+  
+
   const [connecte, setConnecte] = useState(SecureStore.getItem("jwt") != null);
 
   const onConnexion = (data) => {
@@ -31,7 +90,7 @@ export default () => {
       .then((result) => result.json())
       .then((result) => {
         console.log(result.jwt);
-        
+
         SecureStore.setItem("jwt", result.jwt);
         setConnecte(true);
       });
